ed1c159221fc38244c913b2ac0206c28d9dd3915
ryanininertia21 <shrayanbanerjee@gmail.com>
2016-09-18 13:50:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,15 +110,7 @@ app.get('/:articleName', function (req, res) {
   var articleName= req.params.articleName;
   res.send(createtemplate(articles[articleName]));
 });
-app.get('/:articleName', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-app.get('/article-two', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
